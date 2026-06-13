279b97a9ab9adecefd2580489d51b91de1ded288
WEUX Studio <studio@weux.de>
2026-06-13 18:40:49 +0200

    GitHub Pages Deploy + Astro base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://weuxstudio.github.io',
+  base: '/content-finder-site',
   vite: {
     plugins: [tailwindcss()]
   }
